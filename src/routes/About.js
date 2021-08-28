303b19e1b31c65a695e557647fa725416f189110
WhatIsCodin <thinkingmeat@naver.com>
2021-08-28 21:22:35 +0900

    6.1.0

---
+++ src/routes/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ABSOLUTE } from "relateurl";
+
+funtion About(){
+    return <span>About this page: I boult it because I love movies.</span>
+}
+
+export Default About;
